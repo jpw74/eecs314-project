3a0f308893bdfd4bc14c06b39a1c3a267c13e6a8
seslert <tds45@case.edu>
2014-05-01 17:23:24 -0400

    Railfence added into menu.asm functionality. D'GAF.

--- menu.asm
+++ menu.asm
@@ -11,15 +11,16 @@
 	output:			.space		20
 	in_type_prompt:		.asciiz		"Would you like to input from the command line [1] or a file [2]? "
 	out_type_prompt:	.asciiz		"Would you like to write to the command line [1] or a file [2]? "
-	cipher_prompt:		.asciiz		"Select which cipher you would like:\n[1] Caesar\n[2] Affine\n[3] Vigenere\n"
+	cipher_prompt:		.asciiz		"Select which cipher you would like:\n[1] Caesar\n[2] Affine\n[3] Vigenere\n[4] Railfence Transposition\n"
 	string_prompt:		.asciiz		"Enter your string: "
 	not_implemented:	.asciiz		"This feature has not been implemented yet.\n"
 	invalid_input:		.asciiz		"Invalid input.\n"
 	shift_prompt:		.asciiz		"Enter shift amount: "
-	key_prompt:		.asciiz		"Enter keyword (repeat until it matches the length of the plaintext): "
+	key_prompt:			.asciiz		"Enter keyword (repeat until it matches the length of the plaintext): "
+	rail_prompt:		.asciiz 	"\nEnter rail count: \n"
 	in_file_name:		.asciiz		"input.txt"
 	out_file_name:		.asciiz		"output.txt"
-	out_test:		.asciiz		"the fuck"
+	out_test:			.asciiz		"the fuck"
 
 .text
 	jal GetInput		# get input type in $s0, input in $s4
@@ -31,6 +32,7 @@
 	beq $s3, 1, Caesar
 	beq $s3, 2, Affine
 	beq $s3, 3, Vigenere
+	beq $s3, 4, Railfence
 
 CipherReturn:
 	add $t0, $zero, $zero	# initialize loop counter for output
@@ -122,7 +124,7 @@ Vigenere:
 	move $t0, $a0			# move keyword string to register $t0
 	li $t1, 128				# put alphabet size in $t1
 	add $t2, $zero, $zero 	# loop counter in $t2
-
+	#F FALL THROUGH TO VigenereLoop
 VigenereLoop:
 	add $t3, $s4, $t2			# $t3 = address of current byte in plaintext = starting location in plaintext + loop counter
 	add $t4, $t0, $t2			# $t2 = address of current byte in keytext = starting location + loop counter
@@ -142,6 +144,105 @@ VigenereLoop:
 	j VigenereLoop	
 ########## END VIGENERE CIPHER ##########
 
+########## BEGIN RAILFENCE CIPHER ##########
+Railfence:
+	la $a0, rail_prompt	# print shift prompt
+	li $v0, 4
+	syscall
+
+	la $v0, 5				# get rail count
+	syscall
+	
+	move $t0, $v0 			# put rail count in $t0, this is the row count
+	
+	add $t2, $zero, $zero 	# loop/column counter in $t2
+
+Count_Plaintext:
+    add $t1, $s4, $t2					# $t3 = address of current byte in plaintext = starting location + loop/column counter
+    lb $t3, ($t1)						# load byte into $t4
+    beq  $t3, 0, Create_Rail_Fence
+	addi $t2, $t2, 1					# increment counter
+	j Count_Plaintext
+
+Dec_Row_Count:
+	addi $t3, $t3, -1		# decrement row count
+	j RailfenceLoop
+
+Inc_Row_Count:	
+	addi $t3, $t3, 1		# increment row count
+	j RailfenceLoop
+
+Flip_Row_Inc:
+	li $t4, 1
+	j Inc_Row_Count
+
+Flip_Row_Dec:
+	li $t4, 0
+	j Dec_Row_Count
+
+Create_Rail_Fence:
+	addi $t2, $t2, -1 					# take one off length to account for null character
+	move $t1, $t2						# store plaintext length in $s2, this is the column count
+	li $t2, 0							# reset loop counter
+
+	li $t3, 1							# byte count of each cell in 2D array
+	mult $t0, $t1						# multiply array dimensions row/column
+	mflo $a0
+	mult $a0, $t3						# assign byte counts
+	li $v0, 9							# allocate heap memory for array
+	syscall
+	move $s5, $v0						# store array address in $s5 for output in cipher return
+
+	add $t3, $zero, $zero				# initialize row counter
+	add $t4, $zero, $zero				# initialize row state, 1 is increment, 0 is decrement
+
+	#F FALL THROUGH TO RailfenceLoop
+
+RailfenceLoop:
+	add $t5, $s4, $t2					# $t5 = address of current byte = starting location + loop/column counter
+	lb $t6, ($t5)						# load current byte of input into $t6
+	beq $t6, 0, Reset_Counter			# if we reached null characters, quit looping and print array
+
+	# Store in column major order in array at $s3
+	# array address = row + (col * numrows)
+	# $t7 = $t3 + ($t2 * $t0)
+
+	mult $t2, $t0						# multiply current column num by total number of rows
+	mflo $t7							# move result to $t7
+	add $t7, $t7, $t3					# add row count to $t7
+	add $t7, $s5, $t7					# $t7 = address in array for current character
+	sb $t6, ($t7)						# store current byte in array address
+
+	addi $t2, $t2, 1					# increment column counter
+	beq $t3, $t0, Flip_Row_Dec			# reached bottom row
+	beq $t3, 0, Flip_Row_Inc			# reached top row
+	beq $t4, 0, Dec_Row_Count			# if decreasing, decrement
+	beq $t4, 1, Inc_Row_Count			# if increasing, increment
+
+	j RailfenceLoop 						# iterate to next character	
+
+Add_Random:
+	li $a1, 128					# upper bound of random 
+	li $v0, 42					# random int with upper bound
+	syscall
+	sb $a0, ($t5)
+
+	#F FALL THROUGH TO Loop_Array
+
+Loop_Array:
+	add $t5, $s5, $t2			# $t5 = address of current byte in array
+	lb $t6, ($t5)				# load current byte from array into $t6
+	beq $t6, 0, Add_Random 	
+	bgt $t2, $t7, CipherReturn
+	add $t2, $t2, 1				# increment iterator
+	j Loop_Array
+
+Reset_Counter:
+	add $t2, $zero, $zero			# reset loop counter
+	mult $t0, $t1
+	mflo $t7						# total length of array
+	j Loop_Array
+########## END RAILFENCE CIPHER ##########
 
 ########## HELPER ROUTINES HERE TIL END ##########
 GetInput:
@@ -228,7 +329,7 @@ GetCipher:
 	move $s3, $v0		# put cipher type in $s3
 
 	slti $t0, $s3, 1	# if cipher type is less than 1 or greater than 2 => invalid input => start over
-	sgt $t0, $s3, 3
+	sgt $t0, $s3, 4
 	la $t9, GetCipher
 	beq $t8, 1, InvalidInput
 	
